96270e12162f2c5d8f484a93fa17f9b5c617fc0e
Tom Jonker <tjj.jonker@gmail.com>
2023-09-17 17:39:00 -0400

    Created a simple audio program

---
+++ Example01/src/Main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fmod.hpp>
+#include <fmod_errors.h>
+
+
+int main()
+{
+    FMOD_RESULT fm_result;
+    FMOD::System* fm_system;
+    fm_result = FMOD::System_Create(&fm_system);        // Create the main system object.
+
+    if (fm_result != FMOD_OK)
+    {
+        printf("FMOD error! (%d) %s\n", fm_result, FMOD_ErrorString(fm_result));
+        exit(-1);
+    }   
+
+    fm_result = fm_system->init(100, FMOD_INIT_NORMAL, 0);  // Initialize FMOD.
+
+    if (fm_result != FMOD_OK)
+    {
+        printf("FMOD error! (%d) %s\n", fm_result, FMOD_ErrorString(fm_result));
+        exit(-1);
+    }
+
+    FMOD::Sound* sound;
+    fm_result = fm_system->createSound("Resources/Sounds/swoosh.mp3", FMOD_DEFAULT, 0, &sound);
+
+    FMOD::Channel* channel = nullptr;
+    fm_result = fm_system->playSound(sound, 0, false, &channel);
+
+    while (true) {
+        fm_system->update();
+    }
+
+    sound->release();
+    fm_system->release();
+
+}
